7046cadb7163c8f6dce2a320c0344d83f20b0111
eddie <e.jamesduro@gmail.com>
2016-01-22 15:54:46 -0800

    update output for functionality

--- spec/specs.js
+++ spec/specs.js
@@ -1,17 +1,17 @@
 describe('pingPong', function() {
 	it('will return ping for numbers divisible by 3', function(){
-		expect(pingPong(3)).to.equal("ping");
+		expect(pingPong(3)).to.equal("1, 2, ping");
 	});
 
 	it('will return pong for numbers divisible by 5', function(){
-		expect(pingPong(5)).to.equal("pong");
+		expect(pingPong(5)).to.equal("1, 2, ping, 4, pong");
 	});
 
 	it('will return ping pong for numbers divisible by 15', function(){
-		expect(pingPong(15)).to.equal("Ping Pong");
+		expect(pingPong(15)).to.equal("1, 2, ping, 4, pong, ping, 7, 8, ping, pong, 11, ping, 13, 14, Ping Pong");
 	});
 
 	it('will return user input for numbers that are not divisible by 3, 5, or 15', function(){
-		expect(pingPong(1)).to.equal(1);
-	})
+		expect(pingPong(1)).to.be.ok;
+	});
 });
