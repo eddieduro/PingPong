4aaa7a782f66feed97791af1234252f25e2ce543
eddie <e.jamesduro@gmail.com>
2016-01-22 08:50:27 -0800

    add functionality for ping

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var pingPong = function(num) {
+	if(num % 3 === 0){
+		return "ping";
+	}
+}
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe('pingPong', function() {
+	it('will return ping for numbers divisible by 3', function(){
+		expect(pingPong(3)).to.equal("ping");
+	});
+});
