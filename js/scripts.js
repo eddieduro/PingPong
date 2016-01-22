a6b45b0160864df4094ee8a74b6ac60c73097c47
eddie <e.jamesduro@gmail.com>
2016-01-22 15:55:39 -0800

    loop push individual elements into array and returns result as string

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,33 @@
+
 var pingPong = function(num) {
   var ping = "ping";
   var pong = "pong";
   var pingpong = "Ping Pong";
+  var values = [];
 
-	for(var i = 0; i < num; i++) {
-		if(num % 15 === 0) {
-		return pingpong;
-		} else if(num % 3 === 0){
-			return ping;
-		} else if (num % 5 === 0) {
-			return pong;
-		} else {
-			return num;
-		}
-	}
+  for(var i = 1; i <= num; i++) {
+     if (i % 15 === 0) {
+        values.push(pingpong);
+      } else if (i % 3 === 0) {;
+        values.push(ping);
+      } else if (i % 5 === 0) {
+         values.push(pong);
+      } else {
+        values.push(i);
+      }
+    var final = values.join(', ');
+  }
+  return final;  
 }
+	
 
 $(document).ready(function(){
+  $('form#pingpong').submit(function(event){
+    var num = parseInt($('input#number').val());
+    var results = pingPong(num);
 
+
+    $('#results').append(results);
+    event.preventDefault();
+  });
 });
