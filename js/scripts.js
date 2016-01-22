5f8772a8c4ff3cba0561e0fdaf68e602e324d4c7
eddie <e.jamesduro@gmail.com>
2016-01-22 08:52:33 -0800

    add functionality for pong

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 var pingPong = function(num) {
 	if(num % 3 === 0){
 		return "ping";
+	} else if (num % 5 === 0) {
+		return "pong";
 	}
 }
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,8 @@ describe('pingPong', function() {
 	it('will return ping for numbers divisible by 3', function(){
 		expect(pingPong(3)).to.equal("ping");
 	});
+
+	it('will return pong for numbers divisible by 5', function(){
+		expect(pingPong(5)).to.equal("pong");
+	});
 });
