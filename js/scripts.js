4e47ff6e1e2c3f57ea215c7a385f5b56c022b99e
eddie <e.jamesduro@gmail.com>
2016-01-22 08:58:01 -0800

    add loop to pingpong function

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
 var pingPong = function(num) {
-	if(num % 15 === 0) {
+	for(var i = 0; i < num; i++) {
+		if(num % 15 === 0) {
 		return "Ping Pong"
-	} else if(num % 3 === 0){
-		return "ping";
-	} else if (num % 5 === 0) {
-		return "pong";
-	} else {
-		return num;
+		} else if(num % 3 === 0){
+			return "ping";
+		} else if (num % 5 === 0) {
+			return "pong";
+		} else {
+			return num;
+		}
 	}
 }
