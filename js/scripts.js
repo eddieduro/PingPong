a0ffac6dbc1a96191cd6fe9fac12eceb5a0fb2f2
eddie <e.jamesduro@gmail.com>
2016-01-22 10:13:51 -0800

    add ping, pong, pingpong variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,21 @@
 var pingPong = function(num) {
+  var ping = "ping";
+  var pong = "pong";
+  var pingpong = "Ping Pong";
+
 	for(var i = 0; i < num; i++) {
 		if(num % 15 === 0) {
-		return "Ping Pong"
+		return pingpong;
 		} else if(num % 3 === 0){
-			return "ping";
+			return ping;
 		} else if (num % 5 === 0) {
-			return "pong";
+			return pong;
 		} else {
 			return num;
 		}
 	}
 }
+
+$(document).ready(function(){
+
+});
